9b441e6d05851f26b57ef7b552b70c5c49cd66ff
theterminalguy <damiansimonpeter@gmail.com>
2022-11-06 20:39:57 -0500

    Moved gloabls to repo

--- internal/repository/project_repository.go
+++ internal/repository/project_repository.go
@@ -7,26 +7,7 @@ import (
 	"github.com/theterminalguy/writeonce/internal/entity"
 )
 
-var projects []entity.Project
-var templates []entity.Template
-
-func init() {
-	projects = append(projects, entity.Project{
-		ID:          uuid.MustParse("cf4e5443-7f24-4014-8a52-64a53201d1c2"),
-		Name:        "Project 1",
-		Description: "Description 1",
-	})
-
-	templates = append(templates, entity.Template{
-		ID:          uuid.MustParse("928afa3e-e0aa-4eb0-86dd-e70fa4ff961f"),
-		Name:        "Template 1",
-		Description: "Description 1",
-		Template:    "Hello {{.Name}}",
-	})
-}
-
-type ProjectRepository struct {
-}
+type ProjectRepository struct{}
 
 type ProjectParams struct {
 	Name        string `json:"name" validate:"required"`
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,6 +1,36 @@
 package repository
 
-import "github.com/go-playground/validator/v10"
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
+	"github.com/theterminalguy/writeonce/internal/entity"
+	"github.com/theterminalguy/writeonce/internal/template"
+)
+
+var projects []entity.Project
+var templates []entity.Template
+
+func init() {
+	projects = append(projects, entity.Project{
+		ID:          uuid.MustParse("cf4e5443-7f24-4014-8a52-64a53201d1c2"),
+		Name:        "Project 1",
+		Description: "Description 1",
+	})
+
+	templates = append(templates, entity.Template{
+		ID:          uuid.MustParse("928afa3e-e0aa-4eb0-86dd-e70fa4ff961f"),
+		Name:        "Template 1",
+		Description: "Description 1",
+		Template:    "Hello {{.Name}}",
+		ProjectID:   uuid.MustParse("cf4e5443-7f24-4014-8a52-64a53201d1c2"),
+		Placeholders: []template.Placeholder{
+			{
+				Name: "Name",
+				Pos:  8,
+			},
+		},
+	})
+}
 
 func ValidateParams(s interface{}, fields ...string) error {
 	validate := validator.New()
